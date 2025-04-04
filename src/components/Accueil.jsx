0e600d0679797531a4e7dbdd926fe0f572a25a18
Anicet-AGBOSSOU <anicetagbossou5@gmail.com>
2025-04-04 10:44:15 +0100

    [merge] sibar->dev

--- src/components/Accueil.jsx
+++ src/components/Accueil.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React from 'react';
 //import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import './Accueil.css'
+import './Accueil.css';
+
 
 function Accueil() {
   return (
