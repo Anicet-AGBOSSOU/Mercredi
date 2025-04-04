caeb742c018563c47d9c8d79a006a7bb658b9436
Anicet-AGBOSSOU <anicetagbossou5@gmail.com>
2025-04-04 09:54:08 +0100

    [UPDATE]

--- src/main.jsx
+++ src/main.jsx
@@ -4,15 +4,19 @@ import './index.css'
 //import App from './App.jsx';
 import SiBar from './components/partials/SiBar.jsx';
 import NavBar from './components/partials/NavBar.jsx';
-import Footer from './components/partials/Footer.jsx'
-
-
+import Footer from './components/partials/Footer.jsx';
+import Accueil from './components/Accueil.jsx';
+import Form from './components/Form.jsx';
+import Profil from  './components/Profil.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <SiBar />
     <NavBar />
     <Footer />
+    <Accueil />
+    <Form />
+    <Profil />
   </StrictMode>,
 )
 
